e012eb0a0a920527630bf66c2527b3f79214d581
Tim <beswicktim@gmail.com>
2015-10-19 18:17:14 +0100

    Added basic game UI.

--- app/src/main/java/com/example/darkphoton/sungka_project/MainActivity.java
+++ app/src/main/java/com/example/darkphoton/sungka_project/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.darkphoton.sungka_project;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
     }
 
     @Override
