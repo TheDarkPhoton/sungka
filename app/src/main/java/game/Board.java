01c8704af2fd48c65610a91415f125f0f1907486
Max Karasinski <maximilian.karasinski@kcl.ac.uk>
2015-10-20 01:49:57 +0100

    changed an index access and applied rules for capturing shells and getting another turn

--- app/src/main/java/game/Board.java
+++ app/src/main/java/game/Board.java
@@ -4,14 +4,18 @@ package game;
  * An object that describes the state of the current game board
  */
 public class Board {
-    private Cup[] _cups;                                    //an array of board cups
-    //define current player variables here
+    private Cup[] _cups;                                    // an array of board cups
+    private Player _currentPlayer;                          // the player whose turn it is
+    private Player _playerOne;
+    private Player _playerTwo;
 
     /**
      * Constructs board with default attributes.
      */
     Board(Player a, Player b){
         _cups = new Cup[16];
+        _playerOne = a;
+        _playerTwo = b;
 
         //define player a cups
         for (int i = 0; i < 7; i++) {
@@ -22,12 +26,15 @@ public class Board {
         a.bindStore(_cups[7]);
 
         //define player b cups
-        for (int i = 8; i < 16; i++) {
+        for (int i = 8; i < 15; i++) {
             _cups[i] = new ShellCup(7);
             b.bindShellCup(_cups[i], i - 8);
         }
-        _cups[16] = new PlayerCup(a);
-        b.bindStore(_cups[16]);
+        _cups[15] = new PlayerCup(a);
+        b.bindStore(_cups[15]);
+
+        // temporary player assignment
+        _currentPlayer = a;
     }
 
     /**
@@ -47,11 +54,30 @@ public class Board {
             //get the cup object and then increment index
             Cup cup = _cups[index++];
 
-            //if cup is not a player cup then add a shell to that cup
-            if (cup.isNotPlayerCup()){
+            // if cup is not a player cup OR the cup is the player's store,
+            // then add a shell to that cup
+            if (cup.isNotPlayerCup() || _currentPlayer.isStore(cup)) {
                 cup.addShell();
                 --shells;
             }
+        }
+
+        // need to check a few things with the last used cup
+        Cup lastCup = _cups[--index];
+
+        if (lastCup.getCount() == 1 && _currentPlayer.isShellCup(lastCup, index)) {
+
+            // the last shell fell into an empty cup belonging to the player, so capture shells
+            int numShells = lastCup.pickUpShells();
+            numShells += _cups[index % 8].pickUpShells();
+            _currentPlayer.captureShells(numShells);
+        }
+
+        // will possibly change in future
+        if (!_currentPlayer.isStore(lastCup)) {
+
+            // the last cup isn't the current player's store, so switch players
+            _currentPlayer = (_currentPlayer == _playerOne) ? _playerTwo : _playerOne;
         }
     }
 
@@ -76,6 +102,6 @@ public class Board {
      * @return returns player cup object.
      */
     public Cup getPlayerCupB(){
-        return _cups[16];
+        return _cups[15];
     }
 }
