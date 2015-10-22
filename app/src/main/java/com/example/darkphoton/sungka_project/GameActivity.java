46d8f39f126b1f686e6233f729af004960834963
Tim Beswick <beswicktim@gmail.com>
2015-10-22 01:32:33 +0100

    Added shell drawables. Removed default app icon.

--- app/src/main/java/com/example/darkphoton/sungka_project/GameActivity.java
+++ app/src/main/java/com/example/darkphoton/sungka_project/GameActivity.java
@@ -13,8 +13,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
