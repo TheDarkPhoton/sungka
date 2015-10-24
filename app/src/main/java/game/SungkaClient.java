d37752992a7a30f96da5ea12c7fd1fd00c7d530d
Oliver Hohn <oliverh008@hotmail.com>
2015-10-24 23:06:36 +0100

    Added the SungkaClient and SungkaServer classes that will be used in the multiplayer mode of the game. They will be responsible of passing information of moves and the current state of the board between the devices of the Players.

---
+++ app/src/main/java/game/SungkaClient.java
@@ -0,0 +1,10 @@
+package game;
+
+/**
+ * This class represents the Client in the Server-Client relationship. This will allow one Player to connect to anothers
+ * server and thereby be able to play against each other.
+ */
+public class SungkaClient {
+    //TODO:allow the user to connect to a server using a Socket, and pass the information when a move is made to the SungkaServer
+    //TODO:so that the other player can also see it in their screen
+}
---
+++ app/src/main/java/game/SungkaServer.java
@@ -0,0 +1,10 @@
+package game;
+
+/**
+ * This class represents the Server that one Player will set up, so that another Player
+ * can connect to it. Then the Player's will be able to play against each other.
+ */
+public class SungkaServer {
+    //TODO: be able to let a user set up a server, and allow the server to be able handle all the operations
+    //TODO:including passing information of the current state of the board between the Players, as well as their moves
+}
